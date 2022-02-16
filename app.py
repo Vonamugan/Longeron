4f70cd240aa6a07429b54b1d65451f060deb0a90
Vonamugan <vonamugan@gmail.com>
2022-02-16 03:21:09 +0300

    Upgrade db, add post page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
@@ -10,6 +11,9 @@ db = SQLAlchemy(app)
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
+    subtitle = db.Column(db.String)
+    author = db.Column(db.String(50))
+    date_posted = db.Column(db.DateTime)
     body = db.Column(db.Text, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
@@ -21,8 +25,15 @@ class Item(db.Model):
 
 @app.route('/')
 def index():
-    items = Item.query.order_by(Item.id.desc()).all()
-    return render_template('index.html', data=items)
+    items = Item.query.order_by(Item.date_posted.desc()).all()
+    return render_template('index.html', posts=items)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Item.query.filter_by(id=post_id).one()
+
+    return render_template('post.html', post=post)
 
 
 @app.route('/login')
@@ -34,22 +45,18 @@ def login():
 def subscribe():
     return render_template('subscribe.html')
 
-
-@app.route('/post')
-def post():
-    return render_template('post.html')
-
-
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        subtitle = request.form['subtitle']
+        author = request.form['author']
 
-        item = Item(title=title, body=body)
+        post = Item(title=title, body=body, subtitle=subtitle, author=author, date_posted=datetime.now())
 
         try:
-            db.session.add(item)
+            db.session.add(post)
             db.session.commit()
             return redirect('/')
         except:
